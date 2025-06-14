bb0067ffec63c02296d83a73ec75420eb3e88842
Kazuya Takei <myself@attakei.net>
2025-06-15 01:05:50 +0900

    chore: Use LSP from nvim-lspconfig

--- .nvim.lua
+++ .nvim.lua
@@ -1,25 +1,2 @@
-vim.lsp.config('ty', {
-  cmd = { "uv", "run", "ty", "server" },
-  filetypes = { "python" },
-  init_options = {
-    settings = {
-      experimental = {
-        completions = {
-          enable = true
-        }
-      },
-
-      trace = {
-        server = "messages"
-      }
-    }
-  }
-})
-vim.lsp.config('ruff', {
-  cmd = { "uv", "run", "ruff", "server" },
-  filetypes = { "python" },
-})
-
--- Required: Enable the language server
-vim.lsp.enable('ruff')
-vim.lsp.enable('ty')
+vim.lsp.enable("ty")
+vim.lsp.enable("ruff")
