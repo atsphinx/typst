55d950218b58127a1bec869094f0472f3de58fd4
Kazuya Takei <myself@attakei.net>
2025-07-17 01:19:04 +0900

    ci: Add stub of tests
    - These classes are complex to add tests when migration

--- tests/test_core/test_elements/test_model.py
+++ tests/test_core/test_elements/test_model.py
@@ -2,7 +2,71 @@
 # ruff: noqa: D101, D102, D107
 import textwrap
 
+import pytest
+
 from atsphinx.typst.core.elements import model as t
+from atsphinx.typst.core.elements.base import Text
+
+
+class TestHeading:
+    def test_valid_level(self):
+        elm = t.Heading()
+        elm.level = 1
+        Text("Section title", parent=elm)
+        assert elm.to_text() == textwrap.dedent("""\
+#heading(
+  level: 1,
+  [
+    Section title
+  ]
+)""")
+
+    def test_valid_level_2(self):
+        elm = t.Heading()
+        elm.level = 2
+        Text("Section title", parent=elm)
+        assert elm.to_text() == textwrap.dedent("""\
+#heading(
+  level: 2,
+  [
+    Section title
+  ]
+)""")
+
+    def test_invalid_level(self):
+        elm = t.Heading()
+        elm.level = 0
+        assert elm.to_text() == ""
+
+
+class TestBulletList:
+    @pytest.mark.skip(reason="Thinking cases")
+    def test_it(self):
+        pass
+
+
+class TestNumberedList:
+    @pytest.mark.skip(reason="Thinking cases")
+    def test_it(self):
+        pass
+
+
+class TestTable:
+    @pytest.mark.skip(reason="Thinking cases")
+    def test_it(self):
+        pass
+
+
+class TestQuote:
+    @pytest.mark.skip(reason="Thinking cases")
+    def test_it(self):
+        pass
+
+
+class TestFigure:
+    @pytest.mark.skip(reason="Thinking cases")
+    def test_it(self):
+        pass
 
 
 class TestLint:
