ee2c293cfa2adf761d29d494288b0f286c0cc7a7
Kazuya Takei <myself@attakei.net>
2025-07-21 12:53:16 +0900

    ci: Add test for multiple settings for 'typst_documents'

--- tests/test_e2e/test_it.py
+++ tests/test_e2e/test_it.py
@@ -30,3 +30,27 @@ def test__auto_adding_extension(app: SphinxTestApp):
     """Test to pass."""
     app.build()
     assert (app.outdir / "index.typ").exists()
+
+
+@pytest.mark.sphinx(
+    "typst",
+    confoverrides={
+        "typst_documents": [
+            {
+                "entry": "index",
+                "filename": "document-1",
+                "title": "test",
+            },
+            {
+                "entry": "index",
+                "filename": "document-2",
+                "title": "test",
+            },
+        ]
+    },
+)
+def test__multiple_output(app: SphinxTestApp):
+    """Test to pass."""
+    app.build()
+    assert (app.outdir / "document-1.typ").exists()
+    assert (app.outdir / "document-2.typ").exists()
