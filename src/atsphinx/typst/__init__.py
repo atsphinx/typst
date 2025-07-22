1c75cbc363b45fd7035caae6e44ba1fc1b61aca1
Kazuya Takei <myself@attakei.net>
2025-07-22 20:36:50 +0900

    fix: Version management

--- src/atsphinx/typst/__init__.py
+++ src/atsphinx/typst/__init__.py
@@ -7,7 +7,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from sphinx.application import Sphinx
 
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 
 
 def setup(app: Sphinx):  # noqa: D103
