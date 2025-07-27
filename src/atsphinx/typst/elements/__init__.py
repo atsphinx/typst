3f0de38959cc83d4c83136cb409c237406f111c6
Kazuya Takei <myself@attakei.net>
2025-07-27 11:11:08 +0900

    refactor: Rename sub-package

--- src/atsphinx/typst/elements/__init__.py
+++ src/atsphinx/typst/elements/__init__.py
@@ -5,7 +5,7 @@ This module is entrypoint of importing core elements (written in submodules).
 # ruff: noqa: F401
 
 from .base import Element, Source, Text
-from .ext.docutils import Field, Section
+from .custom.docutils import Field, Section
 from .model import (
     BulletList,
     Document,
--- tests/test_elements/test_custom/test_docutils.py
+++ tests/test_elements/test_custom/test_docutils.py
@@ -1,6 +1,6 @@
 # TODO: Write more tests.
 # ruff: noqa: D101, D102, D107
-from atsphinx.typst.elements.ext import docutils as t
+from atsphinx.typst.elements.custom import docutils as t
 
 
 def test_stub():
